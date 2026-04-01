4ac5e3c385accef1671f2e7f48de3b0d20370bdb
chukkala pavan kumar <150168684+pavanchukkala@users.noreply.github.com>
2026-04-01 15:58:05 +0530

    Delete src directory

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { NeonVelocity } from './components/Game/GameContainer';
-
-export default function App() {
-  return (
-    <main className="w-full h-[100dvh] bg-[#050505] overflow-hidden">
-      <NeonVelocity />
-    </main>
-  );
-}
